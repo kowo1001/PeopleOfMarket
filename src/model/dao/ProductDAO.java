8c5c8fb2a746ce82120b845f5cd8aec5d75d7e61
seong-ho <sumgho121@gmail.com>
2020-12-11 15:55:25 +0900

    product update

--- src/model/dao/ProductDAO.java
+++ src/model/dao/ProductDAO.java
@@ -1,5 +1,65 @@
 package model.dao;
 
-public class ProductDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import model.domain.Product;
+
+public class ProductDAO {
+	//create
+	public static Product createProduct(int productNo, String productName, int price, EntityManager em )throws SQLException {
+		EntityTransaction tx = em.getTransaction();	
+		Product p = null;
+
+		tx.begin();
+		p = Product.builder().productNo(productNo).productName(productName).price(price).build();
+		em.persist(p);
+		tx.commit();
+		return p;
+	}
+	//select
+	public static void  findAllProduct (EntityManager em){
+		List<Product> p = em.createNativeQuery("select * from Product", Product.class).getResultList();
+		p.forEach(v -> System.out.println(v));
+	}
+	
+	public static void findProduct(int productNo, EntityManager em)throws SQLException {
+		List<Product> p = em.createNativeQuery("select * from Product where productNo=?", Product.class).setParameter(1, productNo).getResultList();
+		p.forEach(v -> System.out.println(v));
+		if (p.size() == 0) {
+			System.out.println("검색 요청한 제품은 미존재합니다");
+		} ;
+	}
+	
+	public static void findProduct(String productName, EntityManager em)throws SQLException {
+		List<Product> p = em.createNativeQuery("select * from Product where productName=?", Product.class).setParameter(1, productName).getResultList();
+		p.forEach(v -> System.out.println(v));
+		if (p.size() == 0) {
+			System.out.println("검색 요청한 제품은 미존재합니다");
+		} ;
+	}
+	
+	//update
+	public static void updateProduct(String productName, int price, EntityManager em) {
+		int result = em.createNativeQuery("update product set productName = ? where productNo = ?", Product.class).setParameter(1, productName).executeUpdate();
+		if(result !=0) {
+			System.out.println("---업데이트 완료---");
+		}else {
+			System.out.println("---업데이트 실패---");
+		}
+	}	
+	
+	//delete
+	public static void deleteProduct(int productNo, EntityManager em) {
+			int result = em.createNativeQuery("delete from Product where productNo=?", Product.class).setParameter(1,productNo).executeUpdate(); 
+			if(result !=0) {
+				System.out.println("--- 삭제 완료---");
+			}else {
+				System.out.println("--- 삭제 실패---");
+			}
+	}
+	
 }
--- src/model/domain/Product.java
+++ src/model/domain/Product.java
@@ -1,5 +1,31 @@
 package model.domain;
 
-public class Product {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+	@Id
+	
+	@Column(name="pno", precision=20, nullable=false)
+	private int productNo;
+	
+	@Column(name="pname", nullable=false, length=40)
+	private String productName;
+	
+	@Column(name="price", nullable=false)
+	private int price;
+	
+	
 }
