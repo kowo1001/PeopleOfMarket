d59ac3b2cfdf03156b77af3ee8528cc423ba6c90
Ta-Ye <cty1272@naver.com>
2020-12-11 15:27:27 +0900

    마트 entity, DAO 추가

--- src/model/dao/MartDAO.java
+++ src/model/dao/MartDAO.java
@@ -1,5 +1,72 @@
 package model.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+
+import org.junit.jupiter.api.Test;
+
+import model.domain.Mart;
+import model.util.PublicCommon;
+
 public class MartDAO {
 
+	public static Mart createMart(String martName, String location, EntityManager em) throws SQLException {
+		EntityTransaction tx = em.getTransaction();
+		Mart m = null;
+
+		tx.begin();
+		m = Mart.builder().martName(martName).location(location).build();
+		em.persist(m);
+		tx.commit();
+		return m;
+	}
+
+	public static List<Mart> findAll(EntityManager em) throws SQLException, NoResultException {
+		List<Mart> m = em.createNativeQuery("select * from mart", Mart.class).getResultList();
+		return m;
+	}
+
+	public static Mart findMart(int martNumber, EntityManager em) throws SQLException, NoResultException {
+		Mart m = (Mart) em.createNativeQuery("select * from mart where mtno= ?", Mart.class)
+				.setParameter(1, martNumber).getSingleResult();
+		return m;
+	}
+
+	public static Mart findMart(String martName, EntityManager em) throws SQLException, NoResultException {
+		Mart m = (Mart) em.createNativeQuery("select * from Mart where mtname= ?", Mart.class)
+				.setParameter(1, martName).getSingleResult();
+		return m;
+	}
+
+	public static boolean updateMart(int martNumber, String martName, String location, EntityManager em)
+			throws SQLException, NoResultException {
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		Mart m = findMart(martNumber, em);
+		if (m != null) {
+			m.setMartName(martName);
+			m.setLocation(location);
+			tx.commit();
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean deleteMart(int martNumber, EntityManager em) throws SQLException, NoResultException{
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		Mart m = findMart(martNumber, em);
+		if (m != null) {
+			em.remove(m);
+			tx.commit();
+			return true;
+		}
+		return false;
+	}
 }
--- src/model/domain/Mart.java
+++ src/model/domain/Mart.java
@@ -1,5 +1,38 @@
 package model.domain;
 
-public class Mart {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+@Entity
+@SequenceGenerator(name="mart_id_seq_gen", sequenceName="mart_id_seq", initialValue=1, allocationSize=50)
+public class Mart {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mart_id_seq_gen")
+	@Column(name="mtno", precision=10)
+	private int martNumber;
+	
+	@Column(name="mtname", nullable=false, length=40)
+	private String martName;
+	
+	@Column(name="loc", nullable=false, length=40)
+	private String location;
 }
